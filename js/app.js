6c580abdb80273e6e62c77a51ca77530b2c7a8e2
lifeoncode <nduduzo@yellogarden.co.za>
2022-09-10 14:59:46 +0200

    profile layout

--- js/app.js
+++ js/app.js
@@ -17,11 +17,20 @@ searchInput.addEventListener('keyup', (e) => {
             } else {
                 
                 console.log(data.profile)
-                // const output = `
-                // <div class="image">
-                //     <img src="${}" alt="profile avatar"
-                // </div>
-                // `; 
+                const output = `
+                <div class="image">
+                    <img src="${data.avatar_url}" alt="profile avatar"
+                </div>
+                <div class="text">
+                    <span class="username">USERNAME: ${data.login}</span>
+                    <span class="name">NAME: ${data.name}</span>
+                    <span class="bio">BIO: ${data.bio}</span>
+                    <div class="stats">
+                        <span class="followers">FOLLOWERS: <span class="number">${data.followers}</span></span>
+                        <span class="following">FOLLOWING: <span class="number">${data.following}</span></span>
+                    </div>
+                </div>
+                `; 
                 
 
             }
